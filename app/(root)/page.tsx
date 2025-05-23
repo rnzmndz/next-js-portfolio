e8300ed9f206dfa34f58d2aee2350df06d7007b7
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-05-23 20:55:28 +0800

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,34 +4,35 @@ import React from 'react';
 
 function Home() {
   return (
-    <div className="flex flex-col-reverse items-center justify-center lg:flex-row w-full ">
-      <div className="flex gap-10 flex-col pt-10 lg:items-start">
-        <h1 className="text-2xl text-gray-800">Welcome to My Portfolio!</h1>
-        <h3 className="text-4xl">I&apos;M RENZO,</h3>
-        <p className="text-justify max-w-prose lg:max-w-150 text-gray-700">
+    <div className='flex flex-col-reverse items-center justify-center lg:flex-row w-full '>
+      <div className='flex gap-10 flex-col pt-10 lg:items-start'>
+        <h1 className='text-2xl text-gray-800'>Welcome to My Portfolio!</h1>
+        <h3 className='text-4xl'>I&apos;M RENZO,</h3>
+        <p className='text-justify max-w-prose lg:max-w-150 text-gray-700'>
           A Mechanical Engineer transitioning into Full-Stack Web Development.{' '}
           <br /> <br /> I&apos;m passionate about building modern, efficient web
-          applications using Java Spring Boot and Next.js. While I&apos;m still early
-          in my developer journey, I&apos;m constantly learning and growing —
-          preparing to contribute to real-world projects and collaborate with
-          great teams. <br /> <br /> This portfolio is where I share my
-          progress, projects, and professional story. Thanks for stopping by!
+          applications using Java Spring Boot and Next.js. While I&apos;m still
+          early in my developer journey, I&apos;m constantly learning and
+          growing — preparing to contribute to real-world projects and
+          collaborate with great teams. <br /> <br /> This portfolio is where I
+          share my progress, projects, and professional story. Thanks for
+          stopping by!
         </p>
 
         <Link
-          href="/about"
-          className="flex items-center border border-gray-600 py-2 px-6 gap-2 rounded hover:bg-black hover:text-white active:bg-gray-500"
+          href='/about'
+          className='flex items-center border border-gray-600 py-2 px-6 gap-2 rounded hover:bg-black hover:text-white active:bg-gray-500'
         >
           <span>Learn More</span>
         </Link>
       </div>
-      <div className=" lg:block lg:ml-auto p-15 items-center">
+      <div className=' lg:block lg:ml-auto p-15 items-center'>
         <Image
-          className="rounded-full h-auto overflow-hidden outline-1 object-cover shadow-2xl"
-          src="/my-picture.png"
+          className='rounded-full h-auto overflow-hidden outline-1 object-cover shadow-2xl border border-gray-100'
+          src='/my-picture.png'
           width={400}
           height={400}
-          alt="My Picture"
+          alt='My Picture'
         ></Image>
       </div>
     </div>
--- app/(root)/portfolio/page.tsx
+++ app/(root)/portfolio/page.tsx
@@ -1,9 +1,53 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+
+type Portfolio = {
+  link: string;
+  source: string;
+  title: string;
+};
+
+const portfolios: Portfolio[] = [
+  {
+    link: 'https://drive.google.com/drive/folders/1YfcVchKU-py8Wrg_uXmGby2NjMl0-ix4?usp=drive_link',
+    source: '/CAD.jpg',
+    title: 'CAD Works',
+  },
+  {
+    link: 'https://drive.google.com/drive/folders/1mo-R_-_X6K8DoO_h8wbi19ITymlZlsJ8?usp=drive_link',
+    source: '/fire-protection.jpg',
+    title: 'Fire Protection Works',
+  },
+];
+
+function PortfolioSection({ portfolio }: { portfolio: Portfolio }) {
+  return (
+    <div className='relative flex w-full h-64 overflow-hidden items-center justify-center rounded-4xl shadow-lg border border-gray-300'>
+      <Link href={portfolio.link} target='_blank' className='group'>
+        <Image
+          src={portfolio.source}
+          height={700}
+          width={700}
+          alt={portfolio.title}
+          className='w-full h-auto object-cover transition duration-300 group-hover:blur-xs group-hover:scale-110'
+        />
+        <div className='absolute inset-0 flex items-center justify-center'>
+          <span className='text-5xl font-bold bg-opacity-40 rounded-xl bg-gray-100/60 p-2'>
+            {portfolio.title}
+          </span>
+        </div>
+      </Link>
+    </div>
+  );
+}
 
 function Page() {
   return (
-    <div>
-      <h1>Portfolio</h1>
+    <div className='flex flex-col items-center justify-center gap-10 pb-20'>
+      {portfolios.map((portfolio, index) => (
+        <PortfolioSection portfolio={portfolio} key={index} />
+      ))}
     </div>
   );
 }
